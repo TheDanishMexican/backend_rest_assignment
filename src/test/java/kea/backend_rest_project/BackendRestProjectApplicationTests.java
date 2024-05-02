00f8845550b762336193d4796e64d319820f3948
TheDanishMexican <danieljchristjansen@gmail.com>
2024-05-02 15:37:50 +0200

    with the test now

--- src/test/java/kea/backend_rest_project/BackendRestProjectApplicationTests.java
+++ src/test/java/kea/backend_rest_project/BackendRestProjectApplicationTests.java
@@ -2,12 +2,14 @@ package kea.backend_rest_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendRestProjectApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
